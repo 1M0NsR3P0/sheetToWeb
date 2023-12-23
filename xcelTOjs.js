9cac70c2787dd10018e3ef9346ddf99316d137d1
1M0NsR3P0 <naimonchy@gmail.com>
2023-12-23 19:13:30 +0600

    fixed row alignment problem

--- xcelTOjs.js
+++ xcelTOjs.js
@@ -1,43 +1,75 @@
-document.getElementById('excelFile').addEventListener('change', handleFile);
+let loading = false;
 
-/*
-  this file handler will get inputed xls, csv or excel file.
-  create a json file with sheet to json converted lib.
-  after filtering the empty arrays it will append all the cells.
-  finally make css design...
-*/
-
-document.querySelector("#table-container").style.display = "none"
-function handleFile(event) {
-  document.querySelector("#table-container").style.display = "block"
+document.getElementById('excelFile').addEventListener('change', async (event) => {
+  loading = true;
+  document.querySelector("#table-container").style.display = "block";
   document.querySelector("#data").innerHTML = '';
+
   const file = event.target.files[0];
 
-  // file reader to convert xcel to json
+  // Wrap the file reading in a promise
+  const readFile = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (e) => resolve(e.target.result);
+      reader.onerror = (e) => reject(e);
+      reader.readAsArrayBuffer(file);
+    });
+  };
+
   if (file) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      const data = new Uint8Array(e.target.result);
+    try {
+      const data = await readFile(file);
       const workbook = XLSX.read(data, { type: 'array' });
       const sheetName = workbook.SheetNames[0];
       const sheet = workbook.Sheets[sheetName];
-      const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-      
-      const filteredData = jsonData.filter(every=>every.length > 0)
-      filteredData.map(every=>{
+
+      // Get the range of the sheet
+      const range = XLSX.utils.decode_range(sheet['!ref']);
+
+      // Process the data to replace undefined cells with a space
+      const processedData = [];
+      for (let i = range.s.r; i <= range.e.r; i++) {
+        const row = [];
+        for (let j = range.s.c; j <= range.e.c; j++) {
+          const cellAddress = XLSX.utils.encode_cell({ r: i, c: j });
+          const cellValue = sheet[cellAddress] ? sheet[cellAddress].v : undefined;
+          row.push(cellValue === undefined ? " " : cellValue);
+        }
+        if (row.length > 0) {
+          processedData.push(row);
+        }
+      }
+      const filteredData = processedData.filter(row => row.some(cell => cell.trim() !== ""));
+      filteredData.forEach(every => {
         const row = document.createElement('tr');
         every.forEach(element => {
           const cell = document.createElement('td');
           cell.innerText = element;
           row.appendChild(cell);
-          
         });
-        document.querySelector('#data').appendChild(row)
-        
-      })
-
-    };
-
-    reader.readAsArrayBuffer(file);
+        document.querySelector('#data').appendChild(row);
+      });
+    } catch (error) {
+      console.error("Error reading file:", error);
+    } finally {
+      loading = false;
+      console.log(loading);
+    }
   }
+});
+
+
+function format(f = undefined){
+  if(!f){
+
+  }
+  if(f==='size'){
+    document.querySelector(`#data tr`)
+  }
+  if(f==='color'){}
+  if(f==='bunderlineold'){}
+  if(f==='highlight'){}
+  if(f==='bold'){}
+
 }
